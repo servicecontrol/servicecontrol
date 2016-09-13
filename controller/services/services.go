9d0f1506d3e0c64ff14f0e7a0e5cfe3df826f7af
Marc-Steffen Kaesz <mkaesz@servicecontrol.io>
2016-09-13 23:56:19 +0200

    Added function to extract menu data and store it in map for page generation

--- controller/services/services.go
+++ controller/services/services.go
@@ -24,20 +24,10 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	v.Vars["int_name"] = "services"
 	v.Vars["menu_items"] = menu.Config().MenuItems
 	
-	extractPageInfo(v.Vars, menu.Config())	
+	view.ExtractPageInfo(v.Vars, menu.Config())	
 
 	if session.Values["id"] != nil {
 		v.Vars["first_name"] = session.Values["first_name"]
 	}
 	v.Render(w, r)
-}
-
-func extractPageInfo(viewConfig map[string]interface{}, m menu.Menu) {
-	for k,v := range m.MenuItems {
-		fmt.Println(v,k)
-		if v.InternalName == viewConfig["int_name"] {
-			viewConfig["page_title"] = v.PublicName
-			viewConfig["route"] = v.Route
-		}
-	}
 }
