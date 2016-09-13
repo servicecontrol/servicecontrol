bd733b87c1cf8943dd6821b4fce97e94f7955092
Marc-Steffen Kaesz <mkaesz@servicecontrol.io>
2016-09-13 23:52:45 +0200

    Added function to extract menu data and store it in map for page generation

--- controller/services/services.go
+++ controller/services/services.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"net/http"
+	"fmt"
 	"servicecontrol.io/servicecontrol/lib/menu"
 	"servicecontrol.io/servicecontrol/lib/router"
 	"servicecontrol.io/servicecontrol/lib/session"
@@ -22,9 +23,21 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	v := view.New("services/index")
 	v.Vars["int_name"] = "services"
 	v.Vars["menu_items"] = menu.Config().MenuItems
+	
+	extractPageInfo(v.Vars, menu.Config())	
 
 	if session.Values["id"] != nil {
 		v.Vars["first_name"] = session.Values["first_name"]
 	}
 	v.Render(w, r)
+}
+
+func extractPageInfo(viewConfig map[string]interface{}, m menu.Menu) {
+	for k,v := range m.MenuItems {
+		fmt.Println(v,k)
+		if v.InternalName == viewConfig["int_name"] {
+			viewConfig["page_title"] = v.PublicName
+			viewConfig["route"] = v.Route
+		}
+	}
 }
---
+++ lib/menu/menu.go
@@ -0,0 +1,25 @@
+package menu
+
+import ()
+
+var (
+	menu Menu
+)
+
+type Menu struct {
+	MenuItems []MenuItem `json:"MenuItems"`
+}
+
+type MenuItem struct {
+	InternalName string `json:"internalName"`
+	PublicName   string `json:"publicName"`
+	Route        string `json:"route"`
+}
+
+func SetConfig(m Menu) {
+	menu = m
+}
+
+func Config() Menu {
+	return menu
+}
