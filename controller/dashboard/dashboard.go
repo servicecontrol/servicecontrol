4a989c42196c2f3a6bbe23324d005f3502c6e349
Marc-Steffen Kaesz <mkaesz@servicecontrol.io>
2016-09-14 14:52:07 +0200

    refactored dashboard to use consts

--- controller/dashboard/dashboard.go
+++ controller/dashboard/dashboard.go
@@ -2,22 +2,30 @@ package dashboard
 
 import (
 	"net/http"
+
 	"servicecontrol.io/servicecontrol/lib/menu"
 	"servicecontrol.io/servicecontrol/lib/router"
 	"servicecontrol.io/servicecontrol/lib/session"
 	"servicecontrol.io/servicecontrol/lib/view"
 )
 
+const (
+	uri          string = "/"
+	viewTemplate string = "dashboard/index"
+	intName      string = "dashboard"
+)
+
+// Load loads all router for dashboard
 func Load() {
-	router.Get("/", Index)
+	router.Get(uri, Index)
 }
 
+// Index responds to all GET requests
 func Index(w http.ResponseWriter, r *http.Request) {
 	session := session.Instance(r)
 
-	v := view.New("dashboard/index")
-	v.Vars["int_name"] = "dashboard"
-	v.Vars["menu_items"] = menu.Config().MenuItems
+	v := view.New(viewTemplate)
+	v.Vars["int_name"] = intName
 
 	view.ExtractPageInfo(v.Vars, menu.Config())
 
