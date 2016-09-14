1f75ee277348d8f30b2073bbb778e33b3b4e7356
Marc-Steffen Kaesz <mkaesz@servicecontrol.io>
2016-09-14 14:47:27 +0200

    refactored billing to constants

--- controller/billing/billing.go
+++ controller/billing/billing.go
@@ -2,20 +2,32 @@ package billing
 
 import (
 	"net/http"
+
+	"servicecontrol.io/servicecontrol/lib/menu"
 	"servicecontrol.io/servicecontrol/lib/router"
 	"servicecontrol.io/servicecontrol/lib/session"
 	"servicecontrol.io/servicecontrol/lib/view"
 )
 
+const (
+	uri          string = "/billing"
+	viewTemplate string = "billing/index"
+	intName      string = "billing"
+)
+
+// Load loads all routes for billing
 func Load() {
-	router.Get("/billing", Index)
+	router.Get(uri, Index)
 }
 
+// Index answer to all GET requests
 func Index(w http.ResponseWriter, r *http.Request) {
 	session := session.Instance(r)
 
-	v := view.New("billing/index")
-	v.Vars["page_title"] = "Billing"
+	v := view.New(viewTemplate)
+	v.Vars["int_name"] = intName
+
+	view.ExtractPageInfo(v.Vars, menu.Config())
 
 	if session.Values["id"] != nil {
 		v.Vars["first_name"] = session.Values["first_name"]
