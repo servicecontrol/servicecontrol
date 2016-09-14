e1bc81f61fba3506d4ed0d591b497855ac037072
Marc-Steffen Kaesz <mkaesz@servicecontrol.io>
2016-09-14 15:43:47 +0200

    added comments

--- servicecontrol.go
+++ servicecontrol.go
@@ -6,16 +6,22 @@ import (
 	"servicecontrol.io/servicecontrol/lib/server"
 )
 
+// main loads the configuration file, registers the services, applies the
+// middleware to the router, and then starts the HTTP and HTTPS listeners.
 func main() {
+	// Load the configuration file
 	info := boot.LoadConfig("env.json")
 
+	// Register the services
 	boot.RegisterServices(info)
 
+	// Retrieve the middleware
 	handler := boot.SetUpMiddleware(router.Instance())
 
+	// Start the HTTP and HTTPS listeners
 	server.Run(
-		handler,
-		handler,
-		info.Server,
+		handler,     // HTTP handler
+		handler,     // HTTPS handler
+		info.Server, // Server settings
 	)
 }
