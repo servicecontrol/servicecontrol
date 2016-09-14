4090415f9e51b7479ed9841856e88eef4edf04d5
Marc-Steffen Kaesz <mkaesz@servicecontrol.io>
2016-09-14 14:50:10 +0200

    refactores capabilities to constants

--- controller/capabilities/capabilities.go
+++ controller/capabilities/capabilities.go
@@ -2,22 +2,30 @@ package capabilities
 
 import (
 	"net/http"
+
 	"servicecontrol.io/servicecontrol/lib/menu"
 	"servicecontrol.io/servicecontrol/lib/router"
 	"servicecontrol.io/servicecontrol/lib/session"
 	"servicecontrol.io/servicecontrol/lib/view"
 )
 
+const (
+	uri          string = "/capabilities"
+	viewTemplate string = "capabilities/index"
+	intName      string = "capabilities"
+)
+
+// Load loads all routers for capabilities
 func Load() {
-	router.Get("/capabilities", Index)
+	router.Get(uri, Index)
 }
 
+// Index responds to all GET requests
 func Index(w http.ResponseWriter, r *http.Request) {
 	session := session.Instance(r)
 
-	v := view.New("capabilities/index")
-	v.Vars["int_name"] = "capabilities"
-	v.Vars["menu_items"] = menu.Config().MenuItems
+	v := view.New(viewTemplate)
+	v.Vars["int_name"] = intName
 
 	view.ExtractPageInfo(v.Vars, menu.Config())
 
