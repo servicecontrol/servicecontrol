a66b813f261b6605445d0c842c0a46418c9889e7
Marc-Steffen Kaesz <mkaesz@servicecontrol.io>
2016-09-14 14:36:13 +0200

    moved menu items to extract info page

--- lib/view/helper.go
+++ lib/view/helper.go
@@ -4,13 +4,15 @@ import (
 	"servicecontrol.io/servicecontrol/lib/menu"
 )
 
+// ExtractPageInfo extracts page info from app config and adds it to iput map for page rendering
 func ExtractPageInfo(viewConfig map[string]interface{}, m menu.Menu) {
 	for _, v := range m.MenuItems {
 		if v.InternalName == viewConfig["int_name"] {
 			viewConfig["page_title"] = v.PublicName
 			viewConfig["page_route"] = v.Route
 			viewConfig["page_description"] = v.Description
-			viewConfig["isVisibleInMain"] = v.IsVisibleInMain
+			viewConfig["is_visible_in_main_menu"] = v.IsVisibleInMain
+			viewConfig["menu_items"] = m.MenuItems
 		}
 	}
 }
