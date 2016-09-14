d10b960048b91cc56c03318e0ff5463a12cf2b40
Marc-Steffen Kaesz <mkaesz@servicecontrol.io>
2016-09-14 21:34:52 +0200

    fmt

--- controller/logout/logout.go
+++ controller/logout/logout.go
@@ -2,6 +2,7 @@ package logout
 
 import (
 	"net/http"
+
 	"servicecontrol.io/servicecontrol/lib/router"
 	"servicecontrol.io/servicecontrol/lib/session"
 	"servicecontrol.io/servicecontrol/lib/view"
@@ -15,7 +16,6 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	session := session.Instance(r)
 
 	v := view.New("logout/index")
-	v.Vars["page_title"] = "Logout"
 
 	if session.Values["id"] != nil {
 		v.Vars["first_name"] = session.Values["first_name"]
--- controller/register/register.go
+++ controller/register/register.go
@@ -2,6 +2,7 @@ package register
 
 import (
 	"net/http"
+
 	"servicecontrol.io/servicecontrol/lib/router"
 	"servicecontrol.io/servicecontrol/lib/session"
 	"servicecontrol.io/servicecontrol/lib/view"
@@ -15,7 +16,6 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	session := session.Instance(r)
 
 	v := view.New("register/index")
-	v.Vars["page_title"] = "Register"
 
 	if session.Values["id"] != nil {
 		v.Vars["first_name"] = session.Values["first_name"]
