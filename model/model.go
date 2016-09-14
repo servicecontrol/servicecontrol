6f8b23d698ce3a32a2cff75151091d5e7ddc3d95
Marc-Steffen Kaesz <mkaesz@servicecontrol.io>
2016-09-15 00:31:25 +0200

    added standard error for models

--- model/model.go
+++ model/model.go
@@ -1,1 +1,21 @@
+// Package model provides error standardization for the models.
 package model
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var (
+	// ErrNoResult is when no results are found.
+	ErrNoResult = errors.New("Result not found.")
+)
+
+// StandardError returns a model defined error.
+func StandardError(err error) error {
+	if err == sql.ErrNoRows {
+		return ErrNoResult
+	}
+
+	return err
+}
